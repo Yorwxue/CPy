ec8a48cc12c6815b7ce3432c940ff712fd9bd6b4
clliao <yorwxue@gmail.com>
2019-11-13 10:30:27 +0800

    introduce to cmake

--- Readme.md
+++ Readme.md
@@ -43,6 +43,43 @@ return retval;
 ```
 
 ## By Cython
++ Using cdef for c/c++, cpdef for c/c++ and ptrhon, such as:
+```cython 
+cdef public np.ndarray[double, ndim=1] compute(char* path):
+    cdef public int attr 
+    def __cinit__:
+        self.attr = 0
+        // your algorithm go here ..
+```
++ It's necessary to declare types of return object amd initial attributes.
+```cython
+from libcpp.string cimport string
+from libcpp cimport bool
+
+# common types in Cython
+# cdef  int 
+# cdef  float 
+# cdef  string 
+# cdef  object 
+# cdef  list 
+# cdef  bool 
+# cdef YOUR_CUSTOMER_OBJECT 
+```
++ Note that if you using your own type, you're suppose to declare object with right type.
+
+# cmake
++ Copy source files(.c) into ./cmake/src
++ Copy head files(.h) into ./cmake/include
++ Copy shared object files(.so) into ./cmake/ and rename to lib*.so
++ Create CMakeLists.txt. 
++ Note that although you execute the commands in ./cmake/build, but you should define related path based on ./cmake
++ Then, run the following command in ./cmake/:
+```bash
+$ mkdir build
+$ cd build
+$ cmake ../ > /dev/null; make
+```
++ After cmake, you can found executable file with name defined in ./cmake/CMakeLists.txt
 
 
 ### reference: 
@@ -57,3 +94,5 @@ return retval;
 > [9] Numpy type in C(C side): https://docs.scipy.org/doc/numpy/reference/c-api.dtype.html
 > [10] Numpy type in C(Python side): https://numpy.org/devdocs/user/basics.types.html
 > [11] PyArrayObject: https://blog.csdn.net/zj360202/article/details/79214363
+> [12] PyClass for Cython: https://www.reddit.com/r/Cython/comments/8mqlca/convert_python_class_to_cython/
+> [13] Using Python Object in C++: https://www.tutorialspoint.com/How-to-use-Python-object-in-Cplusplus
---
+++ cmake/src/main.c
@@ -0,0 +1,69 @@
+/************************
+* 1. compile by execute the following command:  "gcc $(python3-config --includes) main.c $(python3-config --libs) -o main.o [libXXX.so]"
+* 2. export LD_LIBRARY_PATH=/PATH/TO/DIRECTORY/OF/YOUR/LIBXXX.so
+* 3. run the command: ./cy_c.o
+reference:
+[1] https://medium.com/pyladies-taiwan/%E5%A6%82py%E4%BC%BCc-python-%E8%88%87-c-%E7%9A%84%E5%85%B1%E7%94%9F%E6%B3%95%E5%89%87-568add0ba5b8
+[2] https://blog.csdn.net/zj360202/article/details/79214363
+************************/
+
+#include <Python.h>
+
+/* PATH TO YOUR numpy/core/include/numpy*/
+#include "numpy/arrayobject.h"
+
+#include "pymodule.h"
+
+int show_vector(PyArrayObject *vec);
+int not_doublevector(PyArrayObject *vec);
+
+
+int main() {
+  // Init module & Python compiler
+  PyImport_AppendInittab("pymodule", PyInit_pymodule);
+  Py_Initialize();
+  PyImport_ImportModule("pymodule");
+
+  // call function
+  printf("NPY_FLOAT64: %d\n", 1-not_doublevector(compute("../deer.jpg")));
+  show_vector(compute("../deer.jpg"));
+
+  // release resource used by Python
+  Py_Finalize();
+}
+
+
+int show_vector(PyArrayObject *pyvec){
+
+    double *cvec;
+    int dim, n, m;
+    const char *str;
+
+    /* Check that objects are 'double' type and vectors
+       Not needed if python wrapper function checks before call to this routine */
+    if (NULL == pyvec)  return NULL;
+
+    /* Get vector dimension. */
+    n = PyArray_DIM(pyvec, 0);
+    printf("dimension: %d\n", n);
+
+    /* Change contiguous arrays into C * arrays   */
+    cvec=(double *) PyArray_DATA(pyvec);
+
+    /* show the vector */
+    for(int i=0;i<n;i++){
+        printf("%f ", cvec[i]);
+    }
+    printf("\n");
+
+    return 1;
+}
+
+/* ==== Check that PyArrayObject is a double (Float) type and a vector ==============
+    return 1 if input vector not belong to double type */
+int not_doublevector(PyArrayObject *vec)  {
+//	if (vec->descr->type_num != NPY_DOUBLE || vec->nd != 1)  {
+	if (PyArray_TYPE(vec) != NPY_DOUBLE || PyArray_NDIM(vec) != 1)
+		return 1;
+	return 0;
+}
